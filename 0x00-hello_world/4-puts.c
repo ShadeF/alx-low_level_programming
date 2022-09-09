455846f841ce6a37c2920a7d562e831245f10c72
ShadeF <samanthashade83@gmail.com>
2022-09-09 12:39:40 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * Main - prints a statement
+ * @void: no input arguments
+ *
+ * Description: a function that outputs using puts
+ * Return: execute without errors
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
