74a10172735eb6eaa5c62df79e265339a3b60c63
ShadeF <samanthashade83@gmail.com>
2022-09-10 12:22:20 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 	char alpha = 'a';
+
 	while (alpha <= 'z')
 	{
 		putchar(alpha);
 		alpha++;
 	}
+	putchar('\n');
+	return (0);
 }
