53b17acd037a5e17ae2a06cbe4a82630a128960c
ShadeF <samanthashade83@gmail.com>
2022-09-10 12:51:49 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	char alpha = 'a';
+	
+	while ()
+	{
+		putchar();
+	}
+	putchar('\n');
+	return (0);	
+}
