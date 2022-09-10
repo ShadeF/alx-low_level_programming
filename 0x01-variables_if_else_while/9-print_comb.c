e9a38802ebc13f773ee8eb49f8bf57af9b748b69
ShadeF <samanthashade83@gmail.com>
2022-09-10 13:46:16 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+		
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
