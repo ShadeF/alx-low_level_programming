8f3ffa318c09fbd9a80b51d40d7381a0f0f73cc4
ShadeF <samanthashade83@gmail.com>
2022-09-11 11:54:53 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char alpha = 'a';
 
-	while (alpha == 97 && alpha <123)
+	for (alpha == 97 && alpha < 123)
 	{
-		while (alpha != 101 && alpha != 113)
+		for (alpha != 101 && alpha != 113)
 		{
 			putchar(alpha);
 			alpha++;
