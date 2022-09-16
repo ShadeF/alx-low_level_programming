092d9046f2e1af734b8d354cc50ee1ef4156917f
ShadeF <samanthashade83@gmail.com>
2022-09-16 09:57:53 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - tests to see if the functin will print text
+ * Return: 0
+ */
+int main(void)
+{
+	print_text();
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * print_text - prints the text _putchar
+ * @void: 
+ *
+ * Return: will not return anything
+ */
+void print_text(void);
+void print_text(void)
+{
+	printf("_putchar\n");
+}
+#endif /* MAIN_H */
