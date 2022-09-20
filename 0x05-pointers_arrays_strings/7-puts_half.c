d60d61b029a0f4ac18588d9a9b1c40629085465a
ShadeF <47456548+ShadeF@users.noreply.github.com>
2022-09-20 21:54:12 +0300

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * puts_half - prints half of a string.
- * @str: input string.
- * Return: no return.
+ * puts_half - prints half of a string
+ * @str: input string
+ * Return: no return
  */
 void puts_half(char *str)
 {
